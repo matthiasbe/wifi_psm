9b8d436fa810cc5b42890290c437f2beb118e41f
Matthias Beaupère <matthias.beaupere@ensimag.grenoble-inp.fr>
2017-01-11 15:32:50 +0100

    fiiixxx muzfucka

--- scripts/carte/tether_eternet.sh
+++ scripts/carte/tether_eternet.sh
@@ -1,8 +1,10 @@
 pc_ip_addr=10.42.0.1
+udoo_ip_addr=10.42.0.68
 mask=255.255.255.0
 
-sudo ifconfig eth0 $pc_ip_addr netmask $mask up
+sudo ifconfig eth0 $udoo_ip_addr netmask $mask up
 sudo route add default gw $pc_ip_addr eth0
 
-route
+
+#echo "nameserver $pc_ip_addr" >> /etc/resolvconf/resolv.conf.d/tail
 
